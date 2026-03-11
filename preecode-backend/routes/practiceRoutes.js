49db1819fa60b42c38b2f4cef6bb5dbf97508502
Sahil Saeid <sahilsaeidkhan610@gmail.com>
2026-03-11 10:56:34 +0530

    Remove unused Tailwind CSS configuration and theme management files, along with the white theme logo image.

--- preecode-backend/routes/practiceRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addPractice, getPractice } = require('../controllers/practiceController');
-const auth = require('../middleware/authMiddleware');
-
-router.post('/', auth, addPractice);
-router.get('/', auth, getPractice);
-
-module.exports = router;
